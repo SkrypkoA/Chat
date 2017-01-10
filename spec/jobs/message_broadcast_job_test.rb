e98c4f03e4c61bb3e23aa843c40e1e0a2894b08f
SkrypkoA <skrypko.aleksandr@gmail.com>
2017-01-10 15:54:48 +0200

    Message job test

--- spec/jobs/message_broadcast_job_test.rb
+++ spec/jobs/message_broadcast_job_test.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 
 class MessageBroadcastJobTest
   describe MessageBroadcastJobTest, :type => :job do
-
+    describe "performn later" do
+      it "send message" do
+        ActiveJob::Base.queue_adapter = :test
+        expect{ MessageBroadcastJob.perform_later(Message.last) }.to enqueue_job.on_queue("default")
+      end
+    end
   end
 end
