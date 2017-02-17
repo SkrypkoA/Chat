114275bd726fa4727fa300fa6b7793995790c30a
SkrypkoA <skrypko.aleksandr@gmail.com>
2017-02-17 16:06:43 +0200

    a

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -12,24 +12,24 @@ preload_app true
 
 listen '/tmp/unicorn.chat.sock', backlog: 64
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
-end
+# before_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+#     Process.kill 'QUIT', Process.pid
+#   end
+#
+#   defined?(ActiveRecord::Base) and
+#       ActiveRecord::Base.connection.disconnect!
+# end
+#
+# after_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#   end
+#
+#   defined?(ActiveRecord::Base) and
+#       ActiveRecord::Base.establish_connection
+# end
 
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
