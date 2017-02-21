a083813382b34edf0883033dff2de59c818ded83
SkrypkoA <skrypko.aleksandr@gmail.com>
2017-02-21 12:55:14 +0200

    precompile assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,4 +2,6 @@ CarrierWave.configure do |config|
   config.ignore_integrity_errors = false
   config.ignore_processing_errors = false
   config.ignore_download_errors = false
+  config.permissions = 0666
+  config.directory_permissions = 0777
 end
